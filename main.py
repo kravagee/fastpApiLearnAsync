d8e92c084a9393fb9e217159706fec99e023231a
kravage <kostyash2009@mail.ru>
2025-03-30 22:04:32 +0300

    made architecture, add three endpoints,and add integration with Postgresql DB

---
+++ main.py
@@ -0,0 +1,10 @@
+import uvicorn
+from fastapi import FastAPI
+
+from api.route import main_router
+
+app = FastAPI()
+app.include_router(main_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
